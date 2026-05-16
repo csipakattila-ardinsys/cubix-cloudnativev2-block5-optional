3bbd99bde0a30e37db6c1ae051d3c8d24bbef5a2
CSIPAK Attila <csipak.attila@ardinsys.eu>
2026-05-16 23:15:47 +0200

    Helm: supply APP_DEFAULT_MESSAGE

--- helm/windows/1-setup.ps1
+++ helm/windows/1-setup.ps1
@@ -1,2 +1,2 @@
 kubectl apply -f ..\resources\namespace.yaml
-helm upgrade frontend ..\resources\spring --install -n op5-helm
+helm upgrade frontend ..\resources\spring --install -n op5-helm --values ..\resources\values-custom.yaml
