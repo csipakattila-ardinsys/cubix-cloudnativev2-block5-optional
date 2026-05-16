43d3bd4568bea0454050fe6b05997b468f9ed274
CSIPAK Attila <csipak.attila@ardinsys.eu>
2026-05-16 22:02:59 +0200

    kustomize: use namespaces

--- kustomize/windows/1-setup.ps1
+++ kustomize/windows/1-setup.ps1
@@ -1,4 +1,4 @@
 kubectl apply -f ..\resources\test-namespace.yaml
 kubectl apply -f ..\resources\prod-namespace.yaml
-kubectl apply -k ..\resources\overlays\test
-kubectl apply -k ..\resources\overlays\prod
+kubectl apply -k ..\resources\overlays\test --namespace op5-kustomize-test
+kubectl apply -k ..\resources\overlays\prod --namespace op5-kustomize-prod
